5e880382b9766dcd7fd110a5c5bc33d2b416e0fd
drex15 <mojuenimala@byupathway.edu>
2025-09-07 15:38:22 +0100

    excercise 2 update

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade? ");
+        string totalGrade = Console.ReadLine();
+        int grade = int.Parse(totalGrade);
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is: A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is: B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is: C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is: D");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("Your grade is: F");
+        }
+
+        int gradeSign = grade % 10;
+    
+        
+        if (gradeSign >= 7)
+        {
+            Console.WriteLine("+");
+        }
+        else if (gradeSign < 3)
+        {
+            Console.WriteLine("-");
+        }
+        else
+        {
+            Console.WriteLine("");
+        }
+        Console.WriteLine($"Your grade is {grade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("keep it up next time!");
+        }
     }
+
 }
