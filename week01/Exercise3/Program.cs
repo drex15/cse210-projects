031a259ce6cc3508f89a5c7e17ffa016afb8430c
drex15 <mojuenimala@byupathway.edu>
2025-09-07 16:44:05 +0100

    Excercise 3 updated

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+    
+        }
+
     }
 }
