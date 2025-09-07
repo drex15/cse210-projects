13f20b29dea13f9873175ecae2228ab3e02623a8
drex15 <mojuenimala@byupathway.edu>
2025-09-07 12:57:45 +0100

    Excercise 1 update

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your first name? ");
+        String firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        String lastname = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
